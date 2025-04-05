38a97b168df6e61b4e6a17c216616c8c0c7e6927
Korex23 <ololokes@gmail.com>
2025-04-06 00:30:56 +0100

    build abeg

--- src/app/withdraw/confirm/page.tsx
+++ src/app/withdraw/confirm/page.tsx
@@ -1,16 +1,18 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import Loader from "@/FE/common/Loader";
 
-const Confirm = dynamic(() => import("@/FE/wallet/ConfirmDetails"), {
+// Dynamically import ConfirmDetails with SSR disabled
+const ConfirmDetails = dynamic(() => import("@/FE/wallet/ConfirmDetails"), {
+  ssr: false,
   loading: () => <Loader />,
 });
 
-const Page = () => {
+export default function ConfirmPage() {
   return (
-    <div className="flex justify-center items-center">
-      <Confirm />
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <ConfirmDetails />
     </div>
   );
-};
-
-export default Page;
+}
