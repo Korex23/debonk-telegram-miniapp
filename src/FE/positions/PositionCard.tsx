dd670cd865934955416acf48ad2326055b6328f3
Korex23 <ololokes@gmail.com>
2025-07-11 12:04:32 +0100

    commit

--- src/FE/positions/PositionCard.tsx
+++ src/FE/positions/PositionCard.tsx
@@ -11,39 +11,33 @@ const PositionsCard: React.FC = () => {
   const displayPositions = isSimulation ? positions : realPositions;
 
   return (
-    <main className="bg-[#080808] min-h-screen">
-      <h2 className="text-2xl font-semibold text-center text-white mb-4">
-        Positions
+    <div className="p-2 w-full max-w-[400px] mx-auto">
+      <h2 className="text-2xl font-semibold text-white mb-4">
+        Positions Overview
       </h2>
-      <div className="p-2 w-full max-w-[400px] mx-auto">
-        <h2 className="text-2xl font-semibold text-white mb-4">
-          Positions Overview
-        </h2>
-        <div className="space-y-3 overflow-y-auto max-h-[calc(100vh-150px)]">
-          <div className="space-y-4">
-            {displayPositions.length > 0 ? (
-              <div className="grid grid-cols-1 gap-4">
-                {displayPositions.map((position, index) => (
-                  <PositionCard key={index} position={position} />
-                ))}
-              </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center py-4 text-center rounded-lg border border-[#CC920F]/30 bg-[#2A2A2A]/40 shadow-md">
-                <p className="text-lg font-medium text-[#CC920F]">
-                  No Positions Available
-                </p>
-                <p className="text-sm text-[#CCCCCC] mt-2 p-2">
-                  You don't have any active positions yet. Buy a token to start
-                  tracking.
-                </p>
-              </div>
-            )}
-          </div>
+      <div className="space-y-3 overflow-y-auto max-h-[calc(100vh-150px)]">
+        <div className="space-y-4">
+          {displayPositions.length > 0 ? (
+            <div className="grid grid-cols-1 gap-4">
+              {displayPositions.map((position, index) => (
+                <PositionCard key={index} position={position} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center py-4 text-center rounded-lg border border-[#CC920F]/30 bg-[#2A2A2A]/40 shadow-md">
+              <p className="text-lg font-medium text-[#CC920F]">
+                No Positions Available
+              </p>
+              <p className="text-sm text-[#CCCCCC] mt-2 p-2">
+                You don't have any active positions yet. Buy a token to start
+                tracking.
+              </p>
+            </div>
+          )}
         </div>
       </div>
-
       <BuyPositions />
-    </main>
+    </div>
   );
 };
 
